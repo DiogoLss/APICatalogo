2bc0ace08c07960ab2b2bea9c4ea2ec799c2c280
Diogo Lessa <diogodslessa@gmail.com>
2022-06-13 16:59:09 -0300

    Categoria Repository

---
+++ APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(APICatalogoContext context) : base(context)
+        {
+        }
+        public IEnumerable<Categoria> GetCategoriasProdutos()
+        {
+            return Get().Include(x => x.Produtos);
+        }
+    }
+}
